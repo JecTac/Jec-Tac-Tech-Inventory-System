02456bab5b0db86edd65bf396b2b74e7b25ea59c
JecTac <79200312+JecTac@users.noreply.github.com>
2021-02-21 08:58:38 +0800

    Add files via upload
    Added radio buttons to Request Item page to categorize items to request

---
+++ Jec-Tac-Tech-Inventory-System-main/Jec Tac Tech Inventory management System/src/jec/tac/tech/inventory/management/system/JecTacTechInventoryManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jec.tac.tech.inventory.management.system;
+
+/**
+ *
+ * @author jimmy
+ */
+public class JecTacTechInventoryManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
